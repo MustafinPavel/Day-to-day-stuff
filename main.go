8ec698940d892fd009e71cf669413f6638f10cd9
MustafinPavel <89104507808@mail.ru>
2023-02-22 00:35:26 +0300

    22.02 update

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
 func main() {
+	//input
+	file1, _ := os.Open("input.txt")
+	in := bufio.NewReader(file1)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
 
 }
