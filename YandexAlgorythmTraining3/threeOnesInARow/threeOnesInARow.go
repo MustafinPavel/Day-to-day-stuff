66188703b0f26446ce48c59a156c84aced8252a9
MustafinPavel <89104507808@mail.ru>
2023-02-28 18:08:04 +0300

    28.02 update

--- YandexAlgorythmTraining3/threeOnesInARow/threeOnesInARow.go
+++ YandexAlgorythmTraining3/threeOnesInARow/threeOnesInARow.go
@@ -2,50 +2,43 @@ package threeOnesInARow
 
 import (
 	"bufio"
-	"fmt"
-	"math"
 	"os"
 	"strconv"
-	"strings"
-	"time"
 )
 
 func main() {
 	//ввод
-	timer := time.Now()
 	file1, _ := os.Open("input.txt")
 	in := bufio.NewReader(file1)
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 	var N int64 = readSingleInt(in)
 	//логика
-	result := countVariations(N)
+	var memes []int = make([]int, 40)
+	result := countVariations(N, memes)
 	//вывод
 	out.WriteString(strconv.Itoa(result))
-	fmt.Println(time.Since(timer))
 }
 
-func countVariations(n int64) int {
+func countVariations(n int64, mem []int) int {
 	//база ДП
-	if n == 0 {
+	if mem[n] != 0 {
+		return mem[n]
+	}
+	switch n {
+	case 0:
 		return 0
-	}
-	if n == 1 {
+	case 1:
 		return 2
-	}
-	if n == 2 {
+	case 2:
 		return 4
+	case 3:
+		return 7
 	}
 	//рекуррентность ДП
 	result := 0
-	variants := math.Pow(2, float64(n))
-	for i := variants - 1; i >= variants/2; i-- {
-		numInBin := strconv.FormatInt(int64(i), 2)
-		if !strings.Contains(numInBin, "111") {
-			result++
-		}
-	}
-	result += countVariations(n - 1)
+	result = countVariations(n-1, mem) + countVariations(n-2, mem) + countVariations(n-3, mem)
+	mem[n] = result
 	return result
 }
 
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+)
 
+func main() {
+	//ввод
+	file1, _ := os.Open("input.txt")
+	in := bufio.NewReader(file1)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+	in.Peek(10) //plug
 }
